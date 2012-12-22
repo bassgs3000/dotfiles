42356de8f784a1ecdd5d8acc7f366ea85d3e1170
bassgs3000 <bassgs3000@gmail.com>
2012-12-21 19:17:19 -0600

    Removed zsh stuff from script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim Xdefaults xsession bashrc zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="vimrc vim Xdefaults xsession bashrc"    # list of files/folders to symlink in homedir
 
 ##########
 
